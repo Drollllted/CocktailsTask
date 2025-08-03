7dca276f6f9579d75ca39b09fa6406e6e75898a6
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-03 23:57:03 +0300

    Initial Commit

---
+++ CocktailsApp/CocktailsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsAppApp.swift
+//  CocktailsApp
+//
+//  Created by Drolllted on 03.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
